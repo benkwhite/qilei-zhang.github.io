fcb77c08d3d01d2bdcf54467e3db353a35c84012
benkwhite <benkwhite@hotmail.com>
2023-07-31 21:46:44 -0600

    Post updates

--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "I love New Zealand!",
-        "excerpt":"This is a test post.  ","categories": [],
+        "excerpt":"May trip to New Zealand. I was so lucky to have a chance to visit this beautiful country. I was so impressed by the beautiful scenery and the friendly people there. I will definitely come back again!                                                    The Wanaka Tree Photo by Qilei Zhang       ","categories": [],
         "tags": [],
         "url": "/trip-to-NZ/",
         "teaser": null
